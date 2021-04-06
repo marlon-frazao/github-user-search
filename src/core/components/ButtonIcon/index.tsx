c66915ae62297c9666f269159c9915dd3e311ba9
marlon-frazao <frazao.marlon@gmail.com>
2021-04-06 15:00:03 -0300

    Home page ok. Search page under construction

--- src/core/components/ButtonIcon/index.tsx
+++ src/core/components/ButtonIcon/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { FormEventHandler } from 'react';
 import './styles.scss';
 
 type Props = {
     text: string
+    onClick?: FormEventHandler
 }
 
-const ButtonIcon = ({ text }: Props) => (
+
+const ButtonIcon = ({ text, onClick }: Props) => (
     <div className="d-flex">
-        <button className="btn-icon btn-icon-text">
+        <button className="btn btn-icon" onSubmit={onClick}>
             <h5>{text}</h5>
         </button>
     </div>
