e236be72c49d6e7989c7123a2afdc4cb10a4f7f5
marlon-frazao <frazao.marlon@gmail.com>
2021-04-07 11:29:54 -0300

    Done

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import ButtonIcon from '../../core/components/ButtonIcon';
 import { UserResponse } from '../../core/types/User';
-import { makeRequest } from '../../core/utils/request';
+import DateFormat from './components/DateFormat';
 import ImageLoader from './components/Loaders/ImageLoader';
 import InfoLoader from './components/Loaders/InfoLoader';
+import SimpleInfoCard from './components/SimpleInfoCard';
 import './styles.scss'
 
 
@@ -43,10 +43,32 @@ const Search = () => {
                         userResponse && (
                             <div className="response-card">
                                 <div className="col-3">
-                                    <img src={userResponse.avatar_url} className="user-image" />
+                                    <img src={userResponse.avatar_url} className="user-image" alt="Imagem de usuário" />
                                     <a href={userResponse.html_url}>
                                         <ButtonIcon text="Ver Perfil" />
                                     </a>
+                                </div>
+                                <div className="col-9">
+                                    <div className="superior-card-info">
+                                        <SimpleInfoCard text={`${"Repositórios públicos: "}${userResponse.public_repos}`} />
+                                        <SimpleInfoCard text={`${"Seguidores: "}${userResponse.followers}`} />
+                                        <SimpleInfoCard text={`${"Seguindo: "}${userResponse.following}`} />
+                                    </div>
+                                    <div className="card-info">
+                                        <h3 className="info-text">Informações</h3>
+                                        <div className="text-data">
+                                            <h4>Empresa:&nbsp;</h4><h4 className="text-data-response">{userResponse.company}</h4>
+                                        </div>
+                                        <div className="text-data">
+                                            <h4>Website/Blog:&nbsp;</h4><h4 className="text-data-response">{userResponse.blog}</h4>
+                                        </div>
+                                        <div className="text-data">
+                                            <h4>Localidade:&nbsp;</h4><h4 className="text-data-response">{userResponse.location}</h4>
+                                        </div>
+                                        <div className="text-data">
+                                            <h4>Membro desde:&nbsp;</h4><h4 className="text-data-response"><DateFormat dateString={userResponse.created_at} /></h4>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         )}
